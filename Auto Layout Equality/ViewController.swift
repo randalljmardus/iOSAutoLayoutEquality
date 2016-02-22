88910eabdf84318ddf167c9288f50e93d9d57667
Randall Mardus <randalljmardus@gmail.com>
2016-02-22 14:56:55 -0500

    tested auto layout equality

--- Auto Layout Equality/ViewController.swift
+++ Auto Layout Equality/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    let subview = UIView()
+        subview.backgroundColor = UIColor.blueColor()
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(subview)
+        
+        subview.leftAnchor.constraintEqualToAnchor(view.leftAnchor).active = true
+        subview.widthAnchor.constraintEqualToConstant(100).active = true
+        subview.heightAnchor.constraintEqualToConstant(100).active = true
+        
+        subview.topAnchor.constraintEqualToAnchor(view.topAnchor).active = true
+        view.topAnchor.constraintEqualToAnchor(subview.topAnchor).active = true
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
